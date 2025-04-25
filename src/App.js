951fd66a08abaef2f2e87590fd6f3835f56fc393
gunnuchoudhary <157066570+gunnuchoudhary@users.noreply.github.com>
2025-04-25 15:18:03 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path='/netflix-app' element={<Home />} />
+                <Route path='/' element={<Home />} />
                 <Route path='/tvShows' element={<TvShow />} />
                 <Route path='/movies' element={<Home />} />
             </Routes>
